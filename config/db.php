c2c97fa6bd6bd46e676e25944184e04a167cf6af
derlom <dergrudik@gmail.com>
2019-02-11 23:36:27 +0200

    Yii. install and cinfigured. 1 post migration applied

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=web6yii',
+    'username' => 'mysql',
+//    'tablePrefix' => 'example_',
+    'password' => 'mysql',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
